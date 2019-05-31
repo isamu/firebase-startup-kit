6fc8348f2326192a9a84ed5a0d464e2195670ed9
Isamu Arimoto <isamu@to-kyo.to>
2019-05-31 14:48:27 +0900

    update config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Replace this section with the config file of your own Firebase project. 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Gvm5JpOY6QlignjLSs-V2iNkQo-VVGQ",
-  authDomain: "skelton-us.firebaseapp.com",
-  databaseURL: "https://skelton-us.firebaseio.com",
-  projectId: "skelton-us",
-  storageBucket: "skelton-us.appspot.com",
-  messagingSenderId: "1041354638035",
-  appId: "1:1041354638035:web:9e02a6ea52daf17f"
+  apiKey: "AIzaSyCPsl3JDza-TXcX3YCnGWy7dL1wld8yIko",
+  authDomain: "startkit-test.firebaseapp.com",
+  databaseURL: "https://startkit-test.firebaseio.com",
+  projectId: "startkit-test",
+  storageBucket: "startkit-test.appspot.com",
+  messagingSenderId: "37178205279",
+  appId: "1:37178205279:web:4b997a2195631fa7"
 };
 
 export default firebaseConfig;
